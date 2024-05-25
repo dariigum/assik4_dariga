5c8597cdc2f4286aa161bfabac6f94b719ea0feb
dariigum <darigamusa1@gmail.com>
2024-05-25 21:47:51 +0500

    changed Main

--- src/Main.java
+++ src/Main.java
@@ -1,33 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        MyGraph<String> graph = new MyGraph<>();
+        // Create vertices
+        MyVertex<String> vA = new MyVertex<>("A");
+        MyVertex<String> vB = new MyVertex<>("B");
+        MyVertex<String> vC = new MyVertex<>("C");
+        MyVertex<String> vD = new MyVertex<>("D");
+        MyVertex<String> vE = new MyVertex<>("E");
 
-        graph.addVertex("Almaty");
-        graph.addVertex("Astana");
-        graph.addVertex("Shymkent");
-        graph.addVertex("Pavlodar");
-        graph.addVertex("Oral");
+        // Create graph and add edges
+        MyWeightedGraph<String> graph = new MyWeightedGraph<>();
+        graph.addEdge(vA, vB, 1);
+        graph.addEdge(vA, vC, 4);
+        graph.addEdge(vB, vC, 2);
+        graph.addEdge(vB, vD, 5);
+        graph.addEdge(vC, vE, 3);
+        graph.addEdge(vD, vE, 1);
 
-        graph.addEdge("Almaty","Astana");
-        graph.addEdge("Almaty","Shymkent");
-        graph.addEdge("Astana","Pavlodar");
-        graph.addEdge("Astana","Oral");
-
-
-        System.out.println(graph.hasEdge("Almaty", "Astana"));
-        System.out.println(graph.hasEdge("Almaty", "Pavlodar"));
-
+        // Perform BFS
+        Search<String> bfs = new BreadthFirstSearch<>();
+        System.out.println("BFS Path from A to E:");
+        for (MyVertex<String> vertex : bfs.getPath(vA, vE)) {
+            System.out.print(vertex + " ");
+        }
         System.out.println();
 
-        System.out.println(graph.getNeighbors("Almaty"));
-
-        graph.printGraph();
-        System.out.println("_____________________________");
-        graph.removeEdge("Astana", "Oral");
-
-        graph.printGraph();
-
-
-
+        // Perform Dijkstra
+        Search<String> dijkstra = new DijkstraSearch<>();
+        System.out.println("Dijkstra Path from A to E:");
+        for (MyVertex<String> vertex : dijkstra.getPath(vA, vE)) {
+            System.out.print(vertex + " ");
+        }
+        System.out.println();
     }
 }
